d1937f5bcef64fce869553296b4ec4da5bd54a0f
Thor <43585332+mua105310@users.noreply.github.com>
2023-11-23 09:55:40 +0100

    test mappe gjort klar
    1

---
+++ test/bucket_test.go
@@ -0,0 +1,19 @@
+package test
+
+import "testing"
+
+func DeleteFileTest(t *testing.T) {
+
+}
+
+func DownloadFileTest(t *testing.T) {
+
+}
+
+func ListFileTest(t *testing.T) {
+
+}
+
+func UploadFileTest(t *testing.T) {
+
+}
